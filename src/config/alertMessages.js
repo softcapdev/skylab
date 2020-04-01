3ac396623c2a152cdb8d2024af9350fb4d0b2904
Carlos Passos <carlospassos@MacBook-Pro-de-Carlos.local>
2020-04-01 20:08:58 -0300

    first commit

---
+++ src/config/alertMessages.js
@@ -0,0 +1,7 @@
+export default {
+  PasswordMatch: 'As senhas não conferem!',
+  UserExists: 'Este usuario já está cadastrado no sistema.',
+  TokenNotFound: 'Token not provided',
+  InvalidToken: 'Invalid Token',
+  ValidationFail: 'Validation Fail',
+};
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'a1258d83da08632c0422761df598118e',
+  expiresIn: '7d',
+};
